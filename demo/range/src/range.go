4437fc9f1943f2fcc8abd9a4a0c3a3e3ba4fd12c
ztenv <ztenv@github.com>
2017-06-27 11:04:58 +0800

    修改代码风格

--- demo/range/src/range.go
+++ demo/range/src/range.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
-	var array [10] int 
+func main() {
+	var array [10]int
 	fmt.Println("starting main...")
-	for i:=0;i<10;i++ {
-		array[i]=i
+	for i := 0; i < 10; i++ {
+		array[i] = i
 	}
 
-	for i:=0;i<10;i++{
-		fmt.Printf("array[%d]=%d\n",i,array[i])
+	for i := 0; i < 10; i++ {
+		fmt.Printf("array[%d]=%d\n", i, array[i])
 	}
 
 	fmt.Println("stopped main")
