6ef6cc8fe960c539835ba3552ba92d3efd8de113
ztenv <ztenv@github.com>
2017-07-25 17:35:37 +0800

    wait all goroutine done then exit

--- demo/goroutine/src/goroutine.go
+++ demo/goroutine/src/goroutine.go
@@ -10,8 +10,11 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
+
+var wait_group *sync.WaitGroup = &sync.WaitGroup{}
 
 func main() {
 	var start int = 0
@@ -19,11 +22,12 @@ func main() {
 
 	for i := 0; i < 100; i++ {
 		if i%10 == 0 {
+			wait_group.Add(1)
 			go sum(start, start+step)
 			start += 10
 		}
 	}
-	time.Sleep(1 * time.Second)
+	wait_group.Wait() //wait all goroutine done
 }
 
 /**
@@ -38,7 +42,8 @@ func sum(start, stop int) {
 	var s int = 0
 	for i := start; i < stop; i++ {
 		s += i
-		time.Sleep(1 * time.Millisecond)
+		time.Sleep(1000 * time.Millisecond)
 	}
 	fmt.Printf("sum[%d,%d]=%d\n", start, stop, s)
+	wait_group.Done()
 }
