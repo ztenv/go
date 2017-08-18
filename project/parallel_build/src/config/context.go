cbbdf6e434660428882bc1bd8ee63ff0e910c8f8
ztenv <ztenv@github.com>
2017-08-18 17:15:00 +0800

    补传丢失的文件

--- project/parallel_build/src/config/context.go
+++ project/parallel_build/src/config/context.go
@@ -10,12 +10,14 @@ type Context struct {
 	VCDir    string
 	OutDir   string
 	CPUCount int
+	IsCompileAll string
 }
 
 func (this *Context) Print() {
 	fmt.Printf("Workdir:%s\n", this.WorkDir)
 	fmt.Printf("LibDir:%s\n", this.LibDir)
 	fmt.Printf("VCDir:%s\n", this.VCDir)
-	fmt.Printf("OutDir%s\n", this.OutDir)
+	fmt.Printf("OutDir:%s\n", this.OutDir)
 	fmt.Printf("CPUCount:%d\n", this.CPUCount)
+	fmt.Printf("IsCompileAll:%s\n",this.IsCompileAll)
 }
